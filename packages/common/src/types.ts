e035ba46551850ef2aee4901389d2ae6264f2fbc
cmmohitchau <cmmohitchau@gmail.com>
2025-08-01 11:44:47 +0530

    added common package for urls and zod schemas

---
+++ packages/common/src/types.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+
+export const signupSchema = z.object({
+    name : z.string(),
+    email : z.string().email(),
+    password : z.string().min(4).max(18)
+})
+
+export const signinSchema = z.object({
+    email : z.string().email(),
+    password : z.string()
+})
+
+export const itemSchema = z.object({
+    name : z.string(),
+    initialPrice : z.number(),
+    photo : z.string(),
+    description : z.string()
+})
