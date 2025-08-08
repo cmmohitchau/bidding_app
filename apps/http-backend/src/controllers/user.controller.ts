4bed1a9b00cae1f9853caa0176f9479a4d64e37f
cmmohitchau <cmmohitchau@gmail.com>
2025-08-08 09:37:12 +0530

    added user controller signup signin and allusers

---
+++ apps/http-backend/src/controllers/user.controller.ts
@@ -0,0 +1,115 @@
+import { Request, Response } from "express";
+import { signupSchema , signinSchema , itemSchema } from "@repo/common/types";
+import bcrypt from "bcrypt";
+import { prismaClient } from "@repo/db/client";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/index";
+
+export const signup =  async (req : any , res : Response) => {
+
+    const data = req.body;
+    
+    console.log(data);
+    
+    const parsedData = signupSchema.safeParse(req.body);
+    console.log(parsedData);
+    
+    if(!parsedData.success) {
+        return res.status(401).json({
+            message : "invalid data"
+        })
+    }
+
+    try {
+        const {email , name , password} = parsedData.data;
+
+        const hashedPassword = await bcrypt.hash(password , 10);
+
+        const existingUser = await prismaClient.user.findUnique({
+            where : {
+                email
+            }
+        });
+        
+
+        if(existingUser) {
+            return res.status(401).json({
+                message : "user already exist"
+            })
+        }
+
+        const user = await prismaClient.user.create({
+            data : {
+                email,
+                name,
+                password : hashedPassword,
+                
+            }
+        })
+
+
+        return res.status(200).json({
+            message : "user created successfully"
+        })
+    }   catch (e) {
+        return res.status(501).json({
+            message : "user creation failed"
+        })
+    } 
+}
+
+export const signin = ( async (req : Request , res : Response) => {
+
+    const parsedData = signinSchema.safeParse(req.body);
+
+    if(!parsedData.success) {
+        return res.status(400).json({
+            message : "invalid data"
+        }) 
+    }
+
+    const {email , password} = parsedData.data;
+
+    const existingUser = await prismaClient.user.findUnique({
+        where : {
+            email
+        }
+    })
+    console.log("jwt secret " , JWT_SECRET);
+    
+
+    if(!existingUser) {
+        return res.status(400).json({
+            message : "user does not exist"
+        })
+    }
+
+    const token = jwt.sign({email} as JwtPayload , JWT_SECRET as string);
+    console.log(token);
+    
+    return res.status(200).json({
+        message : "signin successfully",
+        token
+    })    
+})
+
+export const users = ( async (req : Request, res : Response) => {
+
+    try {
+        const body = req.body;
+        console.log(body);
+        
+        const users = await prismaClient.user.findMany();
+
+
+        return res.status(200).json({
+            message : "users fetched successfully",
+            users
+        })    
+    }   catch (e) {
+        return res.status(501).json({
+            message : "Error while fetching user details"
+        })
+    } 
+})
+
