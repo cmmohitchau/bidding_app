8730d6a00c0fd69df984317e55b10367d32b5499
cmmohitchau <cmmohitchau@gmail.com>
2025-08-09 15:02:19 +0530

    added middleware for auth

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,34 @@
+import { JWT_SECRET } from "@repo/backend-common/index";
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      id: string;
+    }
+  }
+}
+export const middleware = async (req : Request , res : Response , next : NextFunction) => {
+
+    try {
+      const token = req.headers["authorization"] ?? "";
+
+      if(!token) return;
+      const verified = jwt.verify(token as string , JWT_SECRET as string) as {userId : string};
+      
+      if(verified.userId) {
+          req.id = verified.userId;
+          next();
+      } else {
+          return res.status(401).json({
+              messagge : "unauthorized token"
+          })
+      }
+    } catch(e) {
+      return res.status(401).json({
+              messagge : "unauthorized token"
+      })
+    }
+
+}
