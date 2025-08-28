689f053ead23a396c713abfdde110bdf76655f1b
cmmohitchau <cmmohitchau@gmail.com>
2025-08-28 09:06:42 +0530

    added get from backend

--- apps/my-app/app/lib/actions/getItems.ts
+++ apps/my-app/app/lib/actions/getItems.ts
@@ -3,12 +3,9 @@ import axios from "axios"
 
 export const getItems = async () => {
     
-    try {
+    
         const res = await axios.get(`${BACKEND_URL}/item/items`);
 
         return res.data.items;
-    } catch(e) {
-        throw new Error("No items are available");
-    }
-    
+
 }
