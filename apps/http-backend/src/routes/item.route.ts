cac285fd3cb81aa849f55449027d0c178b69bae9
cmmohitchau <cmmohitchau@gmail.com>
2025-08-28 13:39:35 +0530

    added route for getting specific item by id

--- apps/http-backend/src/routes/item.route.ts
+++ apps/http-backend/src/routes/item.route.ts
@@ -1,16 +1,18 @@
 
 import { Router } from "express";
-import { addItem, getItem, getItems, getUnsoldItem } from "../controllers/item.controller";
+import { addItem, getItem, getItemById, getItems, getUnsoldItem } from "../controllers/item.controller";
 import { middleware } from "../middleware";
 
 const router : Router = Router();
 
-router.post("/" ,middleware, addItem );
+router.post("/" , middleware , addItem );
 
 router.get("/items" , getItems);
 
 router.get("/available" , getUnsoldItem);
 
 router.get("/" , getItem);
+
+router.get("/:id" , getItemById);
 
 export default router;
