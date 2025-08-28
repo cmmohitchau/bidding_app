a71daf72afae3df2ea09badead198414b630f4b8
cmmohitchau <cmmohitchau@gmail.com>
2025-08-28 09:04:56 +0530

    added the page to show the list of items availble

---
+++ apps/my-app/app/items/page.tsx
@@ -0,0 +1,37 @@
+import { getServerSession } from "next-auth";
+import { ItemsClient } from "../components/pages/ItemsClient";
+import { getItems } from "../lib/actions/getItems";
+import { authOptions } from "../lib/auth/authOption";
+
+
+export interface itemType {
+  id : number,
+  name : string
+  price : number
+  description : string
+  SellerId : string
+  BuyerId? : string | null
+  photo : string
+}
+export default async function Items () {
+    const session = await getServerSession(authOptions);
+      const items = await getItems();
+
+    return(
+        <div>
+            <div className="m-4">
+            {session ? (
+            <div className="text-6xl font-bold flex"><span className="hidden lg:block">Welcome Back ,</span>
+            
+            <span className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent animate-pulse">
+            {session.user?.name}
+            </span>
+            </div>
+            ) : (
+            <p>Please log in</p>
+            )}
+        </div>
+            <ItemsClient items={items} />
+        </div>
+    )
+}
