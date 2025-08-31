17225a2da2c6f3aeabbe1d54db39c03aa178e26c
cmmohitchau <cmmohitchau@gmail.com>
2025-08-31 18:16:00 +0530

    added some types

--- apps/my-app/app/next-auth.d.ts
+++ apps/my-app/app/next-auth.d.ts
@@ -1,4 +1,28 @@
 import NextAuth from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;  
+    user: {
+      id: string;
+      email: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    token?: string; // from backend response
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    uid?: string;
+    email?: string;
+    accessToken?: string; 
+  }
+}
+
 
 declare module "next-auth" {
   interface Session {
@@ -7,12 +31,5 @@ declare module "next-auth" {
       email: string;
       name?: string | null;
     };
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    uid: string;
-    email: string;
   }
 }
