f35148d1962517d7580070d7e031eb53b3323a7f
cmmohitchau <cmmohitchau@gmail.com>
2025-08-31 18:14:37 +0530

    added signin page

--- apps/my-app/app/signin/page.tsx
+++ apps/my-app/app/signin/page.tsx
@@ -47,14 +47,14 @@ export default function SignInPage() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-br from-gray-100 to-gray-200">
       <div className="bg-white shadow-2xl rounded-2xl w-full max-w-md px-8 py-10">
-        <h1 className="text-2xl font-bold text-center mb-6">Welcome Back</h1>
+        <h1 className="text-2xl text-gray-500 font-bold text-center mb-6">Welcome Back</h1>
         <p className="text-gray-500 text-center mb-8">Sign in to your account</p>
 
         {/* Email */}
         <div className="mb-4">
           <label className="text-sm font-medium text-gray-600">Email</label>
           <input
-            className="mt-1 px-3 py-2 w-full border rounded-xl outline-none focus:ring-2 focus:ring-green-400"
+            className="mt-1 px-3 py-2 w-full border rounded-xl text-gray-500 outline-none focus:ring-2 focus:ring-green-400"
             onChange={(e) => setEmail(e.target.value)}
             placeholder="you@example.com"
           />
@@ -65,7 +65,7 @@ export default function SignInPage() {
           <label className="text-sm font-medium text-gray-600">Password</label>
           <input
             onChange={(e) => setPassword(e.target.value)}
-            className="mt-1 px-3 py-2 w-full border rounded-xl outline-none focus:ring-2 focus:ring-green-400"
+            className="mt-1 px-3 py-2 w-full text-gray-500 border rounded-xl outline-none focus:ring-2 focus:ring-green-400"
             type="password"
             placeholder="••••••••"
           />
@@ -101,7 +101,7 @@ export default function SignInPage() {
 
 
           }}
-          className="w-full flex items-center justify-center gap-2 border py-3 rounded-xl hover:bg-gray-100 transition-all"
+          className="w-full flex items-center justify-center gap-2 border py-3 rounded-xl text-gray-500 hover:bg-gray-700 transition-all"
         >
           <Github size={18} />
           Continue with GitHub
