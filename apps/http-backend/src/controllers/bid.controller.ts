0b21734f8f37833333bfc5a2a4dc5aa92c8e2af6
cmmohitchau <cmmohitchau@gmail.com>
2025-09-18 14:48:21 +0530

    added controller for updating sold out to be true and return buyerId

--- apps/http-backend/src/controllers/bid.controller.ts
+++ apps/http-backend/src/controllers/bid.controller.ts
@@ -1,10 +1,24 @@
+import { prismaClient } from "@repo/db/client";
 import { Request, Response } from "express";
 
 
-export const bid = ( (req : Request , res : Response) => {
+export const bid = ( async (req : Request , res : Response) => {
+    const itemId = req.body.itemId;
 
+    const item = await prismaClient.item.update({
+        where : {
+            id : itemId
+        },
+        data : {
+            soldOut : true
+        }
+    })
+    
+    console.log(item.BuyerId);
+    
     
     return res.status(200).json({
-        message : "bid created successfully"
+        message : "bid created successfully",
+        item
     })
 })
