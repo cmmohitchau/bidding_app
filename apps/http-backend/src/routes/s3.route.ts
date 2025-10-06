09c2826b3a0fae07c27bd03258e6815620c840fd
cmmohitchau <cmmohitchau@gmail.com>
2025-10-06 16:22:08 +0530

    changed name of router

--- apps/http-backend/src/routes/s3.route.ts
+++ apps/http-backend/src/routes/s3.route.ts
@@ -2,11 +2,7 @@ import { Router } from "express";
 import { getPresignedUrl, postPresignedUrl } from "../controllers/s3.controller";
 
 const router : Router = Router();
-
 router.get("/get-presigned-url" , getPresignedUrl);
-
-router.get("/post-presigned-url" , postPresignedUrl);
-
-
+router.get("/put-presigned-url" , postPresignedUrl);
 
 export default router;
