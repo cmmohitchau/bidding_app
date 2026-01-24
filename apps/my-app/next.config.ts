9102823dec58359ca6d38833bc233fc657cbbd92
cmmohitchau <cmmohitchau@gmail.com>
2026-01-24 15:21:05 +0530

    added remote image url

--- apps/my-app/next.config.ts
+++ apps/my-app/next.config.ts
@@ -3,8 +3,8 @@ module.exports = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cmmohitchau-bucket1.s3.eu-north-1.amazonaws.com",
-        pathname: "/uploads/**",
+        hostname: "d29c44rupn7bmn.cloudfront.net",
+        pathname: "/**",
       },
     ],
   },
